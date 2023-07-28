92aa6c34eff057708c0e2eb42117e82dfa075a7c
eunnnn9000 <127354285+eunnnn9000@users.noreply.github.com>
2023-07-28 13:29:00 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+$('.navItem' ).css('color','white');
+$('#nav').css('background-color','#145');
+$('#contentContainer ul li:even').css('background-color','#ccc');
+$('#contentContainer ul li:odd').css('background-color','#aaa');
+$('#myInput').val('esther');
